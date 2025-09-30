28466da655aaf7c2a21d9740a32be880c94ab9bc
Sasha Dunaizev <jenderprolite@gmail.com>
2025-09-30 21:33:05 +0500

    Task4

---
+++ Tyuiu.DunaizevAO.Sprint2.Task4.V4.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.DunaizevAO.Sprint2.Task4.V4.Lib
+{
+    public class DataService : ISprint2Task4V4
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = (x+2)<y ? Math.Sin(x) + 2*y : Math.Cos(y) + 2*x*y;
+            double res = Math.Round(z, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.DunaizevAO.Sprint2.Task4.V4/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.DunaizevAO.Sprint2.Task4.V4.Lib;
+
+DataService ds = new DataService();
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y: ");
+double y = double.Parse(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double res = ds.Calculate(x, y);
+Console.WriteLine(res);
