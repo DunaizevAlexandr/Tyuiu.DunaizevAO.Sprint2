97e3bece7f36a11720ebdaa008ea9370d1b89fd6
Sasha Dunaizev <jenderprolite@gmail.com>
2025-09-30 17:14:45 +0500

    Add project files.

---
+++ Tyuiu.DunaizevAO.Sprint2.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.DunaizevAO.Sprint2.Task0.V10.Lib;
+
+namespace Tyuiu.DunaizevAO.Sprint2.Task0.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 1305;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, true, false, true, false, true };
+
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.DunaizevAO.Sprint2.Task0.V10/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.DunaizevAO.Sprint2.Task0.V10.Lib;
+
+DataService ds = new DataService();
+
+int x = 1305;
+int y = 475;
+bool[] result = new bool[6];
+result = ds.GetCompareOperations(x, y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
+}
