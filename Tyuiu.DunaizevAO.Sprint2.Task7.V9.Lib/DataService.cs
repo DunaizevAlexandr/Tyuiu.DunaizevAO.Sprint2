b7aa453a55b7f60029cf5fd42683e00a866b98a5
Sasha Dunaizev <jenderprolite@gmail.com>
2025-10-01 23:24:38 +0500

    Task7

--- Tyuiu.DunaizevAO.Sprint2.Task7.V9.Lib/DataService.cs
+++ Tyuiu.DunaizevAO.Sprint2.Task7.V9.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.DunaizevAO.Sprint2.Task7.V9.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if (((y - Math.Sin(x)) < 0) && (y >= 0 && y <= 0.5) && (x >= 0 && x <= Math.PI))
+            if (((y - Math.Sin(x)) < 0) && (y >= 0 && y <= 0.5) && (x >= 0 && x <= 3.1415))
             {
                 res = true;
             }
